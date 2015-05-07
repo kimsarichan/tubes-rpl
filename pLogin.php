3a85bce7e82e882b6a6b35a5dbeea848244edc39
unknown <dystratos@gmail.com>
2015-05-07 14:09:03 +0700

    edit login

--- pLogin.php
+++ pLogin.php
@@ -3,14 +3,14 @@ session_start();
 include "koneksi.php";
 $nip=$_POST['nip'];
 $password=$_POST['password'];
-$query=mysql_query("select * from perawat where nip='$nip' and password='$password'");
+$query=mysql_query("select * from perawat where nip='$nip' and password='$password' LIMIT 1");
+$data=mysql_fetch_array($query);
 $sum=mysql_num_rows($query);
+$ex=explode("/", $_SERVER['PHP_SELF']);
 if($sum>0){
-	$nip=$_SESSION['nip']=$nip;
-	header ("Location:/sidra/Perawat/index.php"); 
-	echo "aaaa";
+	header ("Location:http://$_SERVER[HTTP_HOST]/$ex[1]/perawat");
 }else{
-	echo "NIP dan Password salah !";
+	header("Location:$_SERVER[HTTP_REFERER]?status=failed");
 }
 
 
