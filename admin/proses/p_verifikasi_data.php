4daeb0586c42338db099c7118c0dd1042f985cb5
unknown <dystratos@gmail.com>
2015-05-08 07:59:22 +0700

    fix admin and login

--- admin/proses/p_verifikasi_data.php
+++ admin/proses/p_verifikasi_data.php
@@ -2,12 +2,13 @@
 include "../../koneksi.php";
 $jabatan=$_POST['jabatan'];
 $masaJabatan=$_POST['masaJabatan'];
-$nip=$_POST['nip']
+$nip=$_POST['nip'];
 $tmpnip=$_POST['tmpnip'];
+$base_url=explode("?", $_SERVER['HTTP_REFERER']);
 $update=mysql_query("update perawat set jabatan='$jabatan', masaJabatan='$masaJabatan', nip='$nip', status=true where nip='$tmpnip'");
 if($update){
-	echo "berhasil";
+	header ("Location: $base_url[0]?pages=verifikasi_perawat&&status=success");
 }else{
-	echo "gagal";
+	header ("Location: $base_url[0]?pages=verifikasi_perawat&&status=failed");
 }
 ?>
