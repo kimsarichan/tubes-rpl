781bb51efc6ef0a2212928f5e4d682aa32c0d31e
kimsarichan <sari.rahmawati11@gmail.com>
2015-05-10 19:52:36 +0700

    input artikel

---
+++ admin/lihat_artikel.php
@@ -0,0 +1,25 @@
+<section id="main-content">
+  <section class="wrapper">
+    <div class="col-lg-12">  
+      <br><br><br>
+               
+        </div><!-- /col-lg-12 -->
+    	<div class="row ">
+    		<div class="col-lg-12">
+    			<div class="form-panel">
+            	  <h4 class="mb"><i class="fa fa-angle-right"></i> Input Artikel</h4>
+                 <form role="form">
+                     <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Masukan nama perawat">
+                    </div>
+                     <textarea type="text" class="form-control " rows="5"></textarea>
+                     <br>
+                     <button type="button" class="btn btn-theme05 " style = "width :150px">Send</button>
+                </form>                   
+    			</div><!-- /form-panel -->
+    		</div><!-- /col-lg-12 -->
+  		
+  	</div><!-- /row -->
+  </section><! --/wrapper -->
+</section><!-- /MAIN CONTENT -->
+  	
