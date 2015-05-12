cdc96c3d3573f8e6faa615e08a10c120ce933dbe
kimsarichan <sari.rahmawati11@gmail.com>
2015-05-12 22:07:32 +0700

    coba artikel , lihat artikel :3

--- lihat_artikel.php
+++ lihat_artikel.php
@@ -39,71 +39,25 @@
     </nav>
     <br><br><br><br>
 
-    <!-- Page Content -->
-    <div class="container">
 
         <div class="row">
+            <div class="col-md-1">
+            </div>
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-                <h1 class="page-header">
-                    Welcome to SIDRA
-                </h1>
+            <div class="col-md-6" style="background-color= white; ">
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="lihat_artikel.php">Title</a>
+                    Title
                 </h2>
                 <p class="lead">
                     by <a href="index.php">Start Bootstrap</a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on 28, 2013 at 10:00 PM</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="lihat_artikel.php">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Second Blog Post -->
-                <h2>
-                    <a href="lihat_artikel.php">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>               
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-                <a class="btn btn-primary" href="lihat_artikel.php">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Older</a>
-                    </li>
-                    <li class="next">
-                        <a href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
-
             </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Cari Artikel</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
+        </div>
             </section>
 </body>
 </html>
